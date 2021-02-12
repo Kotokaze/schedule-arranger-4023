54d3e9d8b16c17b59b03f2a04d26b98144461b81
Kotokaze <5r1j6lvag@relay.firefox.com>
2021-02-12 23:54:29 +0900

    heroku

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ User.sync().then(() => {
 
 
 var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
-var GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -41,7 +41,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.HEROKU_URL ? `${process.env.HEROKU_URL}auth/github/callback` : 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
@@ -94,7 +94,19 @@ app.get('/auth/github',
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   function (req, res) {
-    res.redirect('/');
+    var loginFrom = req.cookies.loginFrom;
+
+    // オープンリダイレクタ脆弱性対策
+    if (
+      loginFrom &&
+      !loginFrom.includes('http://') &&
+      !loginFrom.includes('https://')
+    ) {
+
+      res.clearCookie('loginFrom');
+      res.redirect(loginFrom);
+    }
+    else res.redirect('/');
   });
 
 // catch 404 and forward to error handler
