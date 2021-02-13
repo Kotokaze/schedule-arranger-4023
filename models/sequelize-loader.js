1518d80cf8bbbfd0e3862a63f2e6c853c2e18cc2
Kotokaze <5r1j6lvag@relay.firefox.com>
2021-02-13 10:08:34 +0900

    update

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,6 +1,9 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger');
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
+  { dialectOptions: { ssl: true } }
+);
 
 module.exports = {
   database: sequelize,
